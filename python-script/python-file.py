86019131f327935dea8dbed6f6aaabacf69303be
Nafeesa10 <egum.nafeesa@gmail.com>
2025-08-31 14:41:12 -0500

    add new lines in code

--- python-script/python-file.py
+++ python-script/python-file.py
@@ -1,8 +1,29 @@
+# My first python program
 print("Hello,World")
-print("Python has three numeric types: int, float, and complex")
-myValue=1
-print(myValue)
-print(type(myValue))
-print(str(myValue) + " is of the data type " + str(type(myValue)))
-print(str(myValue) + " is of the data type " + str(type(myValue)))
-myValue=3.14
+#My second python program
+print("python has three numeric types: int,float,complex")
+#creating a variable
+myvalue=1
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type "+ str(type(myvalue)))
+#Introducing the float data type
+myvalue=3.14
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + "is of the data type"+ str(type(myvalue)))
+#introducing the complex data type
+myvalue=5j
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+#introducing the boolan data type for True
+myvalue=True
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + "is of the data type" + str(type(myvalue)))
+#introducing the boolan data type for false
+myvalue=False
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) +" is of the data type" + str(type(myvalue)))
