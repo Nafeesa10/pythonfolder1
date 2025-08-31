e89afb6da40cc85709d6666cc8efd2428a985cd5
Nafeesa10 <egum.nafeesa@gmail.com>
2025-08-31 14:38:27 -0500

    add new line in code

--- python-script/python-file.py
+++ python-script/python-file.py
@@ -4,3 +4,5 @@ myValue=1
 print(myValue)
 print(type(myValue))
 print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=3.14
